a2b3f2ddcb804b5926a2eede7d183202ab743655
M A Monim <ma.monim35@gmail.com>
2020-07-16 12:23:23 +0600

    Updates

---
+++ assets/pages/animate-init.js
@@ -0,0 +1,24 @@
+/*
+ Template Name: Katerack - Bootstrap 4 Admin Dashboard
+ Author: MA Monim
+ File: Animation demo js
+ */
+
+function testAnim(x) {
+    $('#animationSandbox').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass();
+    });
+};
+
+$(document).ready(function() {
+    $('.js--triggerAnimation').click(function(e) {
+        e.preventDefault();
+        var anim = $('.js--animations').val();
+        testAnim(anim);
+    });
+
+    $('.js--animations').change(function() {
+        var anim = $(this).val();
+        testAnim(anim);
+    });
+});
---
+++ precache-manifest.5614529ef21e24b8e0fa8999e09cc660.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f73c31fc5c9cbe5874f24c5b870b1de",
+    "url": "/react-admin-katerack/static/media/icon-plus-gray.1f73c31f.svg"
+  },
+  {
+    "revision": "cd8c695c5a80e5808de9",
+    "url": "/react-admin-katerack/static/js/main.cd8c695c.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-admin-katerack/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f8dc60ed5623b0fa43bc",
+    "url": "/react-admin-katerack/static/js/runtime~main.f8dc60ed.js"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/react-admin-katerack/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/react-admin-katerack/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/react-admin-katerack/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/react-admin-katerack/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-admin-katerack/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8a422611d0bd4451914900a50d4237d5",
+    "url": "/react-admin-katerack/static/media/icon-minus-gray.8a422611.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-admin-katerack/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-admin-katerack/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-admin-katerack/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "0c6ab8331da71a119061",
+    "url": "/react-admin-katerack/static/js/1.0c6ab833.chunk.js"
+  },
+  {
+    "revision": "0c6ab8331da71a119061",
+    "url": "/react-admin-katerack/static/css/1.4ef8a913.chunk.css"
+  },
+  {
+    "revision": "f53db008a205fb0356e6d6ca9e17d8af",
+    "url": "/react-admin-katerack/index.html"
+  }
+];
